6fabbef1249db3234509951940dc47a4964b3f6a
Bekzat Amirbay <bikozade@icloud.com>
2024-02-12 20:53:31 +0000

    passed all 2a tests

--- hw2a/divider_unsigned.sv
+++ hw2a/divider_unsigned.sv
@@ -10,9 +10,30 @@ module divider_unsigned (
     output wire [31:0] o_remainder,
     output wire [31:0] o_quotient
 );
+  logic [31:0] quotient[33], remainder[33], dividend[33];
 
-    // TODO: your code here
+  assign quotient[0] = 0;
+  assign remainder[0] = 0;
+  assign dividend[0] = i_dividend;
 
+  genvar i;
+
+  generate
+    for (i = 0; i < 32; i = i + 1) begin
+      divu_1iter f (
+          .i_dividend (dividend[i]),
+          .i_divisor  (i_divisor),
+          .i_remainder(remainder[i]),
+          .i_quotient (quotient[i]),
+          .o_dividend (dividend[i + 1]),
+          .o_remainder(remainder[i + 1]),
+          .o_quotient (quotient[i + 1])
+      );
+    end
+  endgenerate
+
+  assign o_remainder = remainder[32];
+  assign o_quotient  = quotient[32];
 endmodule
 
 
@@ -25,19 +46,24 @@ module divu_1iter (
     output wire [31:0] o_remainder,
     output wire [31:0] o_quotient
 );
-  /*
-    for (int i = 0; i < 32; i++) {
-        remainder = (remainder << 1) | ((dividend >> 31) & 0x1);
-        if (remainder < divisor) {
-            quotient = (quotient << 1);
-        } else {
-            quotient = (quotient << 1) | 0x1;
-            remainder = remainder - divisor;
-        }
-        dividend = dividend << 1;
-    }
-    */
+  logic [31:0] remainder, quotient, dividend;
+  always_comb begin
+    remainder = (i_remainder << 1) | ((i_dividend >> 31) & 32'b1);
+    quotient  = i_quotient;
+    dividend  = i_dividend;
 
-    // TODO: your code here
+    if (remainder < i_divisor) begin
+      quotient = quotient << 1;
+    end else begin
+      quotient  = (quotient << 1) | 32'b1;
+      remainder = remainder - i_divisor;
+    end
+
+    dividend = dividend << 1;
+  end
+
+  assign o_dividend  = dividend;
+  assign o_remainder = remainder;
+  assign o_quotient  = quotient;
 
 endmodule
